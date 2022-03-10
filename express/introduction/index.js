9013635b40279cfa9e40473ac39e1d6224b8e5b3
diasdev15 <utsmand91@gmail.com>
2022-03-10 13:37:05 +0700

    introduction

--- express/introduction/index.js
+++ express/introduction/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/', (req, res, next) => {
+    res.send('Hello I am learning express')
+    next()
 })
 
 app.listen(port, () => {
