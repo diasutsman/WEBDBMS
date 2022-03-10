66bdb4c4c18776493931b724d87d7a5e1516da75
diasdev15 <utsmand91@gmail.com>
2022-03-10 13:37:32 +0700

    internal & external configuration

---
+++ express/configuration/index.js
@@ -0,0 +1,19 @@
+// internal config
+
+//const express = require('express')
+//const app = express()
+//const port = 3000
+
+//app.get('/', (req, res) => {
+//    res.send('Berhasil mengonfig app')
+//})
+
+//app.get('*', (req, res) => {
+//    res.send('Not Found')
+//})
+
+//app.listen(port, () => {
+//    console.log(`Example app listening on port ${port}`)
+//})
+
+// external config
