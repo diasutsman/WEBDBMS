f5b456c517c8b1104a3f25da232ba99f47f17ead
diasdev15 <utsmand91@gmail.com>
2022-03-10 10:26:31 +0700

    express

--- express/introduction/app.js
+++ express/introduction/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, 'index.html'), err => {
+        if (err) throw err
+    })
 })
 
 app.listen(port, () => {
