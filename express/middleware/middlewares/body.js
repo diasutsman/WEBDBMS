bfd3e578bebb059dc1bceabab595872bd14ed904
diasdev15 <utsmand91@gmail.com>
2022-03-24 10:27:39 +0700

    Middleware

--- express/middleware/middlewares/body.js
+++ express/middleware/middlewares/body.js
@@ -6,7 +6,9 @@ module.exports = (req, res, next) => {
     // jika body kosong maka akan skip middleware ini
     if (isEmpty(req.body)) {
         next()
+    } else {
+        console.log("BODY", req.body)
+        res.send(req.body)
+        next()
     }
-    console.log("BODY", req.body)
-    next()
 }
